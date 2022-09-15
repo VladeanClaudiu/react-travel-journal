2423c4f59bc5ad19f7a85970b932e722da624649
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-09-15 12:51:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from '../components/Navbar'
+import Location from '../components/Location'
+import data from '../data/data'
+
+function App() {
+  const mappedData = data.map((location) => {
+    return <Location
+      key = {location.key}
+      location = {location}
+    />
+
+  })
+  return (
+    <div>
+      <Navbar />
+      <section className='journal-section'>
+        {mappedData}
+      </section>
+    </div>
+  )
+}
+
+export default App
